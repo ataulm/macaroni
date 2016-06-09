021e72845b70a340404fd8c6cc420b7cce9d7750
Ataul Munim <hello@ataulm.com>
2016-06-09 23:19:21 +0100

    Initial commit

---
+++ mobile/src/main/java/com/ataulm/macaroni/StartActivity.java
@@ -0,0 +1,14 @@
+package com.ataulm.macaroni;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class StartActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+}
